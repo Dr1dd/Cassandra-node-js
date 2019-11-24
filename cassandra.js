5cfc43378084aadf73f8959a703a515b462ddc94
Laurynas Leišis <lauris.leisis@gmail.com>
2019-11-24 20:45:08 +0200

    final

--- cassandra.js
+++ cassandra.js
@@ -1,7 +1,7 @@
 const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({ 
-  contactPoints: ['172.24.2.87'],
+  contactPoints: ['192.168.0.105'],
   localDataCenter: 'datacenter1',
   keyspace: 'parking'
 });
@@ -11,107 +11,153 @@ client.connect(function (err) {
   console.log("success");
 });
 
-const queryInsertCustomers = 'INSERT INTO parking.customers (id, ticket_id, name) VALUES (?, ?, ?) IF NOT EXISTS';
-const queryInsertTicket = 'INSERT INTO parking.parking_ticket (id, car_brand, car_model, ticket_cost, checkin_time, validity_duration) VALUES (?, ?, ?, ?, ?, ?) IF NOT EXISTS';
-const queryInsertParking = 'INSERT INTO parking.parking_lot (id, customer_id, city, street) VALUES (?, ?, ?, ?)';
-
-const paramsCustomers = [2, 4, 'John'];
-const paramsTicket = [4, 'BMW', '520', 35, 245, 80];
-const paramsParkingLot = [1, 2, 'Vilnius', 'Naugarduko 24'];
-
-var querySelect = 'SELECT * FROM parking.customers';
-var querySelectParkingLot = 'SELECT * FROM parking.parking_lot WHERE id = 1';
 
 
-// client.execute(querySelect).then(result=>console.log(result));
+
 //createTables();
-//insertData(queryInsertTicket, paramsTicket);
+//insertData();
+//getAllTableData();
 
-//getCustomerById(1);
-//getTicketDataFromCustomers(querySelect);
-//getDataLotsToCustomers(querySelectParkingLot);
-getAllTableData();
+//getTicketsByCustomerId(1);
+//getTicketsByCustomerIdAndTicketId(1, 3);
+//getTicketsByTicketId(1);
 
-//truncateTables();
-//dropTables();
+//getCustomerByParkingLotId(1);
+//getCustomerbyCustId(1);
+
+//getParkingLotInfoById(1);
+
+
 function createTables(){
-  var createCustomersTable = 'CREATE TABLE IF NOT EXISTS parking.customers (id decimal, ticket_id decimal, name text, PRIMARY KEY (id, ticket_id))';
-  var createTicketsTable = 'CREATE TABLE IF NOT EXISTS parking.parking_ticket (id decimal, car_brand text, car_model text, ticket_cost decimal, checkin_time decimal, validity_duration decimal, PRIMARY KEY (id))';
-  var createParkingLotTable ='CREATE TABLE IF NOT EXISTS parking.parking_lot (id decimal, customer_id decimal, city text, street text, PRIMARY KEY (id, customer_id))';
+  var createCustomersTableByPId = 'CREATE TABLE IF NOT EXISTS parking.customers_by_parkingId (p_lot_id decimal, id decimal, name text, PRIMARY KEY (p_lot_id, id))';
+  var createCustomersTableByCustId= 'CREATE TABLE IF NOT EXISTS parking.customers_by_custId (id decimal, p_lot_id decimal, name text, PRIMARY KEY (id, name))';
+ 
+  var createTicketsTableByCustId = 'CREATE TABLE IF NOT EXISTS parking.parking_ticket_by_customerid (customer_id decimal, id decimal, car_brand text, car_model text, ticket_cost decimal, checkin_time decimal, validity_duration decimal, PRIMARY KEY (customer_id, id))';
+  var createTicketsTableByCustIdandTicketId = 'CREATE TABLE IF NOT EXISTS parking.parking_ticket_by_customerid_and_ticketId (customer_id decimal, id decimal, car_brand text, car_model text, ticket_cost decimal, checkin_time decimal, validity_duration decimal, PRIMARY KEY ((customer_id, id), ticket_cost))';
+  var createTicketsTableByTicketId = 'CREATE TABLE IF NOT EXISTS parking.parking_ticket_by_ticketid (id decimal, customer_id decimal, car_brand text, car_model text, ticket_cost decimal, checkin_time decimal, validity_duration decimal, PRIMARY KEY (id, customer_id))';
+ 
+  var createParkingLotTable ='CREATE TABLE IF NOT EXISTS parking.parking_lot_by_id (p_lot_id decimal, city text, street text, PRIMARY KEY (p_lot_id))';
+  
+  client.execute(createCustomersTableByPId);
+  client.execute(createCustomersTableByCustId);
 
-  client.execute(createCustomersTable);
-  client.execute(createTicketsTable);
+  client.execute(createTicketsTableByCustId);
+  client.execute(createTicketsTableByCustIdandTicketId);
+  client.execute(createTicketsTableByTicketId);
+
   client.execute(createParkingLotTable);
 
 }
 function insertData(query, params){
-    client.execute(query, params, { prepare: true }).then(result => console.log(result));
+  //FOR CUSTOMERS TABLE
+  const queryInsertCustomers = 'INSERT INTO parking.customers_by_parkingId (p_lot_id, id, name) VALUES (?, ?, ?) IF NOT EXISTS';
+  const queryInsertCustomersByCustId = 'INSERT INTO parking.customers_by_custId (p_lot_id, id, name) VALUES (?, ?, ?) IF NOT EXISTS';
+  
+   //FOR TICKET TABLE
+  const queryInsertTicket = 'INSERT INTO parking.parking_ticket_by_customerid (customer_id, id, car_brand, car_model, ticket_cost, checkin_time, validity_duration) VALUES (?, ?, ?, ?, ?, ?, ?) IF NOT EXISTS';
+  const queryInsertTicketByCIandTId = 'INSERT INTO parking.parking_ticket_by_customerid_and_ticketId(customer_id, id, car_brand, car_model, ticket_cost, checkin_time, validity_duration) VALUES (?, ?, ?, ?, ?, ?, ?) IF NOT EXISTS';
+  const queryInsertTicketByTId= 'INSERT INTO parking.parking_ticket_by_ticketid(id, customer_id, car_brand, car_model, ticket_cost, checkin_time, validity_duration) VALUES (?, ?, ?, ?, ?, ?, ?) IF NOT EXISTS';
+  
+   //FOR PARKING LOT TABLE
+  const queryInsertParking = 'INSERT INTO parking.parking_lot_by_id (p_lot_id, city, street) VALUES (?, ?, ?) IF NOT EXISTS';
+
+   //PARAMETERS FOR CUSTOMERS TABLE
+  const paramsCustomers = [1, 1, 'John'];
+  const paramsCustomersByCustId = [1, 1, 'John'];
+
+   //PARAMETERS FOR TICKET TABLE
+  const paramsTicket = [1, 3, 'Volkswagen', 'Jetta', 315, 25, 15];
+  const paramsTicketIdsSwapped = [3, 1, 'Volkswagen', 'Jetta', 315, 25, 15];
+
+   //PARAMETERS FOR PARKING LOT TABLE
+  const paramsParkingLot = [1, 'Vilnius', 'Naugarduko 24'];
+
+  //FOR CUSTOMERS TABLE
+  client.execute(queryInsertCustomers, paramsCustomers, { prepare: true }).then(result => console.log(result));
+  client.execute(queryInsertCustomersByCustId, paramsCustomersByCustId, { prepare: true }).then(result => console.log(result));
+  
+  //FOR TICKET TABLE
+  client.execute(queryInsertTicket, paramsTicket, { prepare: true }).then(result => console.log(result));
+  client.execute(queryInsertTicketByCIandTId, paramsTicket, { prepare: true }).then(result => console.log(result));
+  client.execute(queryInsertTicketByTId, paramsTicketIdsSwapped, { prepare: true }).then(result => console.log(result));
+    
+  //FOR PARKING LOT TABLE
+  client.execute(queryInsertParking, paramsParkingLot, { prepare: true }).then(result => console.log(result));
 
 }
-// function getData(query, ticket_id){
-//   client.execute(query)
-//     .then(result => {
 
-//       console.log("id  car_brand  car_model  checkin_time  ticket_cost  validity_duration");
-
-//         //console.log('User has ticket: %s', result.rows[i].ticket_id);
-//         var queryTicket = 'SELECT * FROM parking.parking_ticket WHERE id = ?';
-//         var queryParam = [result.rows[ticket_id].ticket_id];
-//         client.execute(queryTicket, queryParam).then(result => {
-//           console.log(result.rows[0].id + " " +result.rows[0].car_brand + " " + result.rows[0].car_model + " " + result.rows[0].checkin_time + " " + result.rows[0].ticket_cost + " " + result.rows[0].validity_duration);
-//         });
-//     }
-// }
-function getCustomerById(customer_id){
-  var query = 'SELECT * FROM parking.customers WHERE id = '+customer_id;
+//TICKET QUERIES
+function getTicketsByCustomerId(customer_id){
+  var query = 'SELECT * FROM parking.parking_ticket_by_customerid WHERE customer_id = '+customer_id+'ORDER BY id';
   client.execute(query).then(result => {
-    var ticketArray = [];
-    for(let i = 0; i < result.rowLength;i++){
-      ticketArray.push(result.rows[i].ticket_id);
-    }
-      console.log("id | Ticket ids |  name");
-      console.log(result.rows[0].id + " | "+ticketArray + " | " + result.rows[0].name);
+       console.log("Customer id | Ticket id | car_brand | car_model | checkin_time | ticket_cost | validity_duration");
+      for(let i = 0; i < result.rowLength;i++){
+        console.log(result.rows[i].customer_id + " | "+result.rows[i].id + " | " +result.rows[i].car_brand + " | " + result.rows[i].car_model + " | " + result.rows[i].checkin_time + " | " + result.rows[i].ticket_cost + " | " + result.rows[i].validity_duration);
+     }
+  });
+
+}
+function getTicketsByTicketId(ticket_id){
+   var query = 'SELECT * FROM parking.parking_ticket_by_ticketid WHERE id = '+ticket_id+'ORDER BY customer_id';
+  client.execute(query).then(result => {
+       console.log("Ticket id | Customer id | car_brand | car_model | checkin_time | ticket_cost | validity_duration");
+      for(let i = 0; i < result.rowLength;i++){
+        console.log(result.rows[i].id + " | "+result.rows[i].customer_id + " | " +result.rows[i].car_brand + " | " + result.rows[i].car_model + " | " + result.rows[i].checkin_time + " | " + result.rows[i].ticket_cost + " | " + result.rows[i].validity_duration);
+     }
   });
 }
-function getTicketDataFromCustomers(query){
-  client.execute(query)
-    .then(result => {
-      console.log("id  car_brand  car_model  checkin_time  ticket_cost  validity_duration");
-      for (let i = 0; i < result.rowLength; i++){
-
-        //console.log('User has ticket: %s', result.rows[i].ticket_id);
-        var queryTicket = 'SELECT * FROM parking.parking_ticket WHERE id = ?';
-        var queryParam = [result.rows[i].ticket_id];
-        client.execute(queryTicket, queryParam).then(result => {
-          console.log(result.rows[0].id + " " +result.rows[0].car_brand + " " + result.rows[0].car_model + " " + result.rows[0].checkin_time + " " + result.rows[0].ticket_cost + " " + result.rows[0].validity_duration);
-        });
-      }
-    });
-}
-function getDataLotsToCustomers(query){
-  client.execute(query)
-    .then(result => {
-      console.log("id  customer_id  city  street");
-      for (let i = 0; i < result.rowLength; i++){
-        console.log(result.rows[i].id + " "+result.rows[i].customer_id + " " + result.rows[i].city+ " "+ result.rows[i].street);
-        var queryCustomer = 'SELECT * FROM parking.customers WHERE id = ?';
-        var queryParam = [result.rows[i].customer_id];
-        client.execute(queryCustomer, queryParam).then(result => {
-           console.log(result.rows[0].id + " " +result.rows[0].ticket_id + " " + result.rows[0].name);
-
-      });
-    }
+function getTicketsByCustomerIdAndTicketId(customer_id, ticket_id){
+  var query = 'SELECT * FROM parking.parking_ticket_by_customerid_and_ticketId WHERE customer_id = '+customer_id+' AND id = '+ticket_id+' ORDER BY ticket_cost';
+  client.execute(query).then(result => {
+       console.log("Customer id | Ticket id | car_brand | car_model | checkin_time | ticket_cost | validity_duration");
+      for(let i = 0; i < result.rowLength;i++){
+        console.log(result.rows[i].customer_id + " | "+result.rows[i].id + " | " +result.rows[i].car_brand + " | " + result.rows[i].car_model + " | " + result.rows[i].checkin_time + " | " + result.rows[i].ticket_cost + " | " + result.rows[i].validity_duration);
+     }
   });
 }
+
+//CUSTOMER QUERIES
+function getCustomerByParkingLotId(p_lot_id){
+  var query = 'SELECT * FROM parking.customers_by_parkingId WHERE p_lot_id = '+p_lot_id+' ORDER BY id';
+  client.execute(query).then(result => {
+      console.log("Parking lot id | Customer id |  Name");
+      for(let i = 0; i < result.rowLength;i++){
+        console.log(result.rows[i].p_lot_id + " | "+result.rows[i].id + " | " + result.rows[i].name);
+     }
+  });
+}
+function getCustomerbyCustId(customer_id){
+   var query = 'SELECT * FROM parking.customers_by_custId WHERE id = '+customer_id+' ORDER BY name';
+  client.execute(query).then(result => {
+      console.log("Customer ID | Parking lot id |  Name");
+      for(let i = 0; i < result.rowLength;i++){
+        console.log(result.rows[i].id + " | "+result.rows[i].p_lot_id + " | " + result.rows[i].name);
+     }
+  });
+}
+
+//PARKING LOT QUERIES
+function getParkingLotInfoById(p_lot_id) {
+    var query = 'SELECT * FROM parking.parking_lot_by_id WHERE p_lot_id = '+p_lot_id;
+  client.execute(query).then(result => {
+      console.log("Parking lot id | City |  Street");
+      for(let i = 0; i < result.rowLength;i++){
+        console.log(result.rows[i].p_lot_id + " | "+result.rows[i].city + " | " + result.rows[i].street);
+     }
+  });
+}
+
+
+//OTHER
 function getAllTableData() {
-  var queryCust = 'SELECT * FROM parking.customers';
-  var queryTicket = 'SELECT * FROM parking.parking_ticket';
-  var queryParking = 'SELECT * FROM parking.parking_lot';
+  var queryCust = 'SELECT * FROM parking.customers_by_parkingId';
+  var queryTicket = 'SELECT * FROM parking.parking_ticket_by_customerid';
+  var queryParking = 'SELECT * FROM parking.parking_lot_by_id';
   client.execute(queryCust).then(result=>{
   console.log("Customers Table");
-  console.log("id | ticket id | name");
+  console.log("Parking lot id | customer id | name");
     for(let i = 0 ; i < result.rowLength; i++){
-      var jsonCustomers = { id: result.rows[i].id, ticketId: result.rows[i].ticket_id, name: result.rows[i].name };
+      var jsonCustomers = { parking_lot_id: result.rows[i].p_lot_id, customer_id: result.rows[i].id, name: result.rows[i].name };
       var str = JSON.stringify(jsonCustomers, null, 2);
       console.log(str);
     }
@@ -120,46 +166,25 @@ function getAllTableData() {
   });
     client.execute(queryTicket).then(result=>{
     console.log("Ticket Table");
-    console.log("id | car bran | car model | checkin time | validityduration");
+    console.log("customer id | ticked id | car brand | car model | checkin time | validityduration");
     for(let i = 0 ; i < result.rowLength; i++){
-      var jsonTicket = { id: result.rows[i].id, car_brand: result.rows[i].ticket_id, car_model: result.rows[i].name, checkin_time: result.rows[i].checkin_time, ticket_cost: result.rows[i].ticket_cost, validity_duration: result.rows[i].validity_duration };
+      var jsonTicket = { customer_id: result.rows[i].customer_id, ticket_id: result.rows[i].id, car_brand: result.rows[i].ticket_id, car_model: result.rows[i].name, checkin_time: result.rows[i].checkin_time, ticket_cost: result.rows[i].ticket_cost, validity_duration: result.rows[i].validity_duration };
       var str = JSON.stringify(jsonTicket, null, 2);
       console.log(str);
     }
     console.log("");
 
   });
-      client.execute(queryParking).then(result=>{
+    client.execute(queryParking).then(result=>{
     console.log("Parking Lot Table");
-    console.log("id | customer id | city | street");
+    console.log("Parking lot id | city | street");
     for(let i = 0 ; i < result.rowLength; i++){
-      var jsonParking = { id: result.rows[i].id, customer_id: result.rows[i].customer_id, city: result.rows[i].city, street: result.rows[i].street };
+      var jsonParking = { p_lot_id: result.rows[i].id, city: result.rows[i].city, street: result.rows[i].street };
       var str = JSON.stringify(jsonParking, null, 2);
       console.log(str);
-      //console.log(result.rows[i].id + " | " + result.rows[i].customer_id + " | " + result.rows[i].city + " | "+ result.rows[i].street);
     }
     console.log("");
   });
 
-
-}
-function truncateTables(){
-  var truncateCustomers = 'TRUNCATE TABLE parking.customers';
-  var truncateTickets = 'TRUNCATE TABLE parking.parking_ticket';
-  var truncateParkingLot = 'TRUNCATE TABLE parking.parking_lot';
-
-  client.execute(truncateCustomers);
-  client.execute(truncateTickets);
-  client.execute(truncateParkingLot);
-
-}
-function dropTables(){
-  var dropCustomers = 'DROP TABLE IF EXISTS parking.customers';
-  var dropTickets = 'DROP TABLE IF EXISTS parking.parking_ticket';
-  var dropParkingLot = 'DROP TABLE IF EXISTS parking.parking_lot';
-
-  client.execute(dropCustomers);
-  client.execute(dropTickets);
-  client.execute(dropParkingLot);
 
 }
